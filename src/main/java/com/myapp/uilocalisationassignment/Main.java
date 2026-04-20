f217c2f3f5e1a8ac6f88a9afbc7254e1c7043db0
azuru <saloarttu@outlook.com>
2026-04-20 20:14:45 +0300

    Fix static access in main

--- src/main/java/com/myapp/uilocalisationassignment/Main.java
+++ src/main/java/com/myapp/uilocalisationassignment/Main.java
@@ -1,7 +1,9 @@
 package com.myapp.uilocalisationassignment;
 
+import javafx.application.Application;
+
 public class Main {
     public static void main(String[] args) {
-        AppGUI.launch(AppGUI.class, args);
+        Application.launch(AppGUI.class, args);
     }
 }
