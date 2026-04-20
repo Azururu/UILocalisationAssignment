a88bf1c63e06f7dbaa6c203f930697edd6e31751
azuru <saloarttu@outlook.com>
2026-04-20 20:06:53 +0300

    Add tests for AppGUITest

---
+++ src/test/java/com/myapp/uilocalisationassignment/AppGUITest.java
@@ -0,0 +1,39 @@
+package com.myapp.uilocalisationassignment;
+
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppGUITest extends ApplicationTest {
+
+    private AppGUI app;
+    private Stage stage;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.stage = stage;
+        app = new AppGUI();
+        app.start(stage);
+    }
+
+    @Test
+    void testAppStartsAndTitleIsSet() {
+        assertEquals("Fuel Cost Calculator", stage.getTitle(), "The stage title should be 'Fuel Cost Calculator'");
+    }
+
+    @Test
+    void testSceneIsInitialized() {
+        assertNotNull(stage.getScene(), "The scene should not be null");
+        assertNotNull(stage.getScene().getRoot(), "The scene root should not be null");
+        assertEquals(400.0, stage.getScene().getWidth(), 0.01, "The scene width should be 400");
+        assertEquals(500.0, stage.getScene().getHeight(), 0.01, "The scene height should be 500");
+    }
+
+    @Test
+    void testRootElementIsPresent() {
+        // We can lookup the root VBox defined in the FXML
+        assertNotNull(lookup("#rootVBox").query(), "The root VBox with fx:id 'rootVBox' should be present");
+    }
+}
